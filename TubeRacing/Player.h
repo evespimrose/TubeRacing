f106eeb34559544b1cfd46780f1bab796cd2c0e6
kcjsend2 <kcjsend2@naver.com>
2020-11-14 10:00:26 +0900

    기본 구조 생성

--- TubeRacing/Player.h
+++ TubeRacing/Player.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "framework.h"
+
+class Player
+{
+private:
+	//방향 벡터
+	glm::vec3 dirVec;
+
+	//위치값
+	float xpos;
+	float ypos;
+	float zpos;
+
+	//위치 행렬, 회전 행렬
+	glm::mat4 PosMat;
+	glm::mat4 RotMat;
+
+	GLuint VAO;
+	GLuint VBO;
+	GLuint EBO;
+
+public:
+	Player();
+
+};
